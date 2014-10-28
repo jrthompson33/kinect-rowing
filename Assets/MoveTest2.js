b85f05091caeef46432965be05f80c6689d005b1
Lisa Li <lisali90319@gmail.com>
2014-10-28 22:29:50 +0800

    Move Boat and Camera
    1. Add Javascript to Boat
    2. Put "Main Camera" as a child of "Boat"

---
+++ Assets/MoveTest2.js
@@ -0,0 +1,23 @@
+﻿#pragma strict
+
+var speed : float = 0.2;
+function Update () {
+	
+	if (Input.GetKey("w")){
+		this.transform.position.z += this.speed;
+		}
+	
+	if (Input.GetKey("s")){
+		this.transform.position.x -= this.speed;
+		}
+	
+	if (Input.GetKey("a")){
+		this.transform.position.z -= this.speed;
+		}
+		
+	if (Input.GetKey("d")){
+		this.transform.position.x += this.speed;
+		}
+
+
+}

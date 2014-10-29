d75e5b8331adebcc0bed2aca214f65ba557a4956
jrthompson33 <jrthompson@gatech.edu>
2014-10-29 02:39:14 -0400

    Fixing the water interactions for the boat.
    Created the buoyancy script which gives the boat floating like
    properties. Also needed to create a specific mesh collider for the boat
    so that script worked properly on the kayak form (needed to be a
    completely closed off shape).
    
    Added water effects to the boat with the mesh particle emitter
    component (named it kayak spray). The spray could still use work, but
    it’s working well enough for now.

---
+++ Assets/Scripts/BoatController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoatController : MonoBehaviour {
+	
+	public Boundary boundary;
+	public float speed;
+	public float tilt;
+	
+	[System.Serializable]
+	public class Boundary {
+		public float xMin, xMax, zMin, zMax;
+	}
+	
+	// Executed once per physics step
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		
+		Vector3 move = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rigidbody.velocity += move * speed;
+		
+		float x = Mathf.Clamp(rigidbody.position.x, boundary.xMin, boundary.xMax);
+		float y = rigidbody.position.y;
+		float z = Mathf.Clamp(rigidbody.position.z, boundary.zMin, boundary.zMax);
+
+		//TODO see if you can change it so that we rotate the direction of the boat before we move in that direction
+		
+		rigidbody.position = new Vector3 (x, y, z);
+	}
+}

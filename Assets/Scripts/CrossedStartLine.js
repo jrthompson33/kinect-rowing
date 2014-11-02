f117022e8faade569d1362eea8f4f65400d01d59
Varsha <varshu21@gmail.com>
2014-11-02 01:06:25 -0500

    Added start and finish line triggers & time taken
    1. Trigger scripts for start and finish lines
              CrossedFinishLine.js
              CrossedStartLine.js
    
    2. Display the total time taken

---
+++ Assets/Scripts/CrossedStartLine.js
@@ -0,0 +1,13 @@
+﻿//Script to log the start line trigger
+
+static var startTime = 0;
+
+function OnTriggerEnter(other: Collider)
+{
+	if(other.CompareTag("Boat"))
+	{
+		Debug.Log("Crossed the start line " + other.name);
+		startTime = Time.time;
+	}
+}
+

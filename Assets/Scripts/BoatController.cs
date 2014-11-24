a2b845e284c2bb22315c9418acd5f70a1c161d60
jrthompson33 <jrthompson@gatech.edu>
2014-11-24 15:16:53 -0500

    Animation for kayaker
    Added the animation and controls for the kayaker. Also got rid of the
    other random objects (boxes, rowboats, etc.).
    Moved the start line to increase the duration of the game.

--- Assets/Scripts/BoatController.cs
+++ Assets/Scripts/BoatController.cs
@@ -3,30 +3,58 @@ using System.Collections;
 
 public class BoatController : MonoBehaviour {
 	
+	public GameObject kayaker;
+	private Animator animator;
+	
 	public Boundary boundary;
 	public float speed;
-	public float tilt;
+	public float turn;
+	public float adjust;
 	
 	[System.Serializable]
 	public class Boundary {
 		public float xMin, xMax, zMin, zMax;
 	}
 	
+	void Start () 
+	{
+		animator = kayaker.GetComponent<Animator>();
+		Debug.Log("Direction starts at " + animator.GetInteger("Direction"));
+	}
+	
+	void Update() 
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		
+		if (animator) {
+			//set event parameters based on user input
+			if(moveHorizontal > 0.7f) {
+				animator.SetInteger("Direction", 1);
+			} else if (moveHorizontal < -0.7f) {
+				animator.SetInteger("Direction", -1);
+			} else {
+				animator.SetInteger("Direction", 0);
+			}
+		}  
+		Debug.Log ("Horizontal is " + moveHorizontal);
+	}
+	
 	// Executed once per physics step
 	void FixedUpdate()
 	{
 		float moveHorizontal = Input.GetAxis ("Horizontal");
-		float moveVertical = Input.GetAxis ("Vertical");
+		//		float moveVertical = Input.GetAxis ("Vertical");
 		
-		Vector3 move = new Vector3 (moveHorizontal, 0.0f, moveVertical);
-		rigidbody.velocity += move * speed;
+		if (moveHorizontal != 0) 
+		{
+			// Rotate by -1 to start because for some reason the boats mesh causes it go side-ways
+			transform.Rotate(this.transform.up, adjust);
+			this.rigidbody.AddForce(this.transform.forward * speed, ForceMode.Force);
+			transform.Rotate(this.transform.up, moveHorizontal * turn);
+		}
 		
 		float x = Mathf.Clamp(rigidbody.position.x, boundary.xMin, boundary.xMax);
 		float y = rigidbody.position.y;
 		float z = Mathf.Clamp(rigidbody.position.z, boundary.zMin, boundary.zMax);
-
-		//TODO see if you can change it so that we rotate the direction of the boat before we move in that direction
-		
-		rigidbody.position = new Vector3 (x, y, z);
 	}
 }
